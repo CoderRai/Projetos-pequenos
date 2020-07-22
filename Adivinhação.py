aa6746858db4b855a11a302a69382c99b60f6601
Raí Lincoln <developer.rai30@gmail.com>
2020-07-22 16:20:22 -0300

    Create Adivinhação.py

---
+++ Adivinhação.py
@@ -0,0 +1,20 @@
+from random import randint
+
+NUMBER = randint(0, 10)
+ATTEMPTS = 3
+C = 0
+
+# repetir enquanto houver tentativas
+while C < 3:
+
+    KICK = int(input('Em que número de 1 a 10 o computador pensou?'))
+    C += 1
+    ATTEMPTS -= 1
+
+    if KICK == NUMBER:
+        print("Parabêns você acertou")
+
+    elif KICK != NUMBER:
+        print("Você errou!!Você tem {} chances".format(ATTEMPTS))
+
+print("O número é: {}\n FIM".format(NUMBER))
